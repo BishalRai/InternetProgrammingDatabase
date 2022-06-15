be3b170cbca151f1b1232a8b83261ab542fb6638
Bishal Rai <bishal6193@gmail.com>
2022-06-15 07:51:57 +0545

    database section commented

--- js_basic/ex2.js
+++ js_basic/ex2.js
@@ -3,9 +3,11 @@ const bookList = ["Book 1","Book 2","Book 3"];
 const book = {
     getAllBooks(){
         console.log(bookList);
+        //select * from book;
     },
     addNewBook(NameOFNewBook){
         console.log("New Book " + NameOFNewBook + " has been added.");
+        //insert into book
     }
 }
 
